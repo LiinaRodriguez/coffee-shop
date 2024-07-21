3c6c65a399f2f59036ff135fa60235e1a74b99bc
LinaRodriguez <lsrodriguez@unillanos.edu.co>
2024-07-21 10:42:00 -0500

    Add: display all products ☕

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for coffee_shop project.
+URL configuration for coffee_shop project products app.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,10 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from .views import ProductListView
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('products/', include('products.urls'))
+    path('allproducts/', ProductListView.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
+from .models import Product
+from django.views.generic.base import  TemplateView
 from django.shortcuts import render
 # Create your views here.
 
+class ProductListView(TemplateView):
+    template_name = 'products/products_list.html'
+
+    def get_context_data(self):
+        product_list = Product.objects.all()
+        return {'products_list': product_list}
