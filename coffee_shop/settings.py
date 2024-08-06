ffb87e14b9d875b8f901389f5746f76beea15891
LinaRodriguez <lsrodriguez@unillanos.edu.co>
2024-08-05 22:17:42 -0500

    add: orders 🧾

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -150,3 +150,4 @@ CRISPY_TEMPLATE_PACK = "tailwind"
 
 LOGIN_REDIRECT_URL =  'welcome'
 LOGOUT_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
--- orders/urls.py
+++ orders/urls.py
@@ -1,3 +1,7 @@
+from django.urls import path
+from .views import MyOrderView, CreateOrderProductView
+
 urlpatterns = [
-    
+    path('my-order',MyOrderView.as_view(), name='myorder'),
+    path('add-product', CreateOrderProductView.as_view(), name='add_order_product')
 ]
